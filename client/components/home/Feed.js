bbd0b1d79fd98ac5b4a114e323878878658046d8
Tanish <tanishbaansal@gmail.com>
2022-06-27 02:46:00 +0530

    Added Feed Boxes with test content

--- client/components/home/Feed.js
+++ client/components/home/Feed.js
@@ -1,11 +1,79 @@
 import { BsStars } from "react-icons/bs";
 import TweetBox from "./TweetBox";
+import Post from "./Post";
 
 const style = {
     wrapper: `flex-[2] border-r border-l border-[#38444d]`,
     header: `sticky top-0 bg-[#15202b] z-10 p-4 flex justify-between items-center`,
     headerTitle: `text-xl font-bold`,
 };
+
+const tweets = [
+    {
+        displayName: "Tanish",
+        userName: "0x8chdgertherhtrehtj23213213",
+        avatar: "https://avatars.githubusercontent.com/u/61084415",
+        text: "gm",
+        isProfileImageNft: false,
+        timestamp: "2020-06-01T12:00:00.000Z",
+    },
+    {
+        displayName: "Tanish",
+        userName: "0x8chdgertherhtrehtj23213213",
+        avatar: "https://avatars.githubusercontent.com/u/61084415",
+        text: "gm",
+        isProfileImageNft: false,
+        timestamp: "2020-06-01T12:00:00.000Z",
+    },
+    {
+        displayName: "Tanish",
+        userName: "0x8chdgertherhtrehtj23213213",
+        avatar: "https://avatars.githubusercontent.com/u/61084415",
+        text: "gm",
+        isProfileImageNft: false,
+        timestamp: "2020-06-01T12:00:00.000Z",
+    },
+    {
+        displayName: "Tanish",
+        userName: "0x8chdgertherhtrehtj23213213",
+        avatar: "https://avatars.githubusercontent.com/u/61084415",
+        text: "gm",
+        isProfileImageNft: false,
+        timestamp: "2020-06-01T12:00:00.000Z",
+    },
+    {
+        displayName: "Tanish",
+        userName: "0x8chdgertherhtrehtj23213213",
+        avatar: "https://avatars.githubusercontent.com/u/61084415",
+        text: "gm",
+        isProfileImageNft: false,
+        timestamp: "2020-06-01T12:00:00.000Z",
+    },
+    {
+        displayName: "Tanish",
+        userName: "0x8chdgertherhtrehtj23213213",
+        avatar: "https://avatars.githubusercontent.com/u/61084415",
+        text: "gm",
+        isProfileImageNft: false,
+        timestamp: "2020-06-01T12:00:00.000Z",
+    },
+    {
+        displayName: "Tanish",
+        userName: "0x8chdgertherhtrehtj23213213",
+        avatar: "https://avatars.githubusercontent.com/u/61084415",
+        text: "gm",
+        isProfileImageNft: false,
+        timestamp: "2020-06-01T12:00:00.000Z",
+    },
+    {
+        displayName: "Tanish",
+        userName: "0x8chdgertherhtrehtj23213213",
+        avatar: "https://avatars.githubusercontent.com/u/61084415",
+        text: "gm",
+        isProfileImageNft: false,
+        timestamp: "2020-06-01T12:00:00.000Z",
+    },
+];
 
 function Feed() {
     return (
@@ -15,6 +83,20 @@ function Feed() {
                 <BsStars />
             </div>
             <TweetBox />
+            {tweets.map((tweet, index) => (
+                <Post
+                    key={index}
+                    displayName={tweet.displayName}
+                    userName={`${tweet.userName.slice(
+                        0,
+                        4
+                    )}...${tweet.userName.slice(-4)}`}
+                    avatar={tweet.avatar}
+                    text={tweet.text}
+                    isProfileImageNft={tweet.isProfileImageNft}
+                    timestamp={tweet.timestamp}
+                />
+            ))}
         </div>
     );
 }
